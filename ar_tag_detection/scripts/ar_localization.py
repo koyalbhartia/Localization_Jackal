2741409e7526ea5daca2eee399ee64d25665ef03
harshkakashaniya <harshkakashaniya@github.com>
2019-11-17 00:43:26 -0500

    Dev: odom publisher created

---
+++ ar_tag_detection/scripts/ar_localization.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# import required packages
+import rospy, math, os
+import numpy as np
+from geometry_msgs.msg import Point, PoseStamped, PoseWithCovarianceStamped, Twist , Pose2D
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from std_msgs.msg import Bool
+from nav_msgs.msg import Odometry
+import rospkg
+import yaml
+
+class Tags():
+       
+    def __init__(self):
+        self.tag_target=PoseStamped()
+        self.X=None
+        self.Y=None
+        self.TZ=None
+        
+        rospy.init_node("AR_localization")
+
+        #Publish on /AR_target_pose ,topic as a PoseStamped message
+        self.tag_pub = rospy.Publisher("AR_target_pose", Odometry, queue_size=10)
+
+        #Subscribe pose from amcl_pose
+        rospy.Subscriber("amcl", PoseWithCovarianceStamped, self.get_turtlePose)
+
+        #subscribe pose from alvar markers
+        rospy.Subscriber("ar_pose_marker", AlvarMarkers, self.get_tags)
+        rospy.loginfo("Publishing marker pose for particular tag id")
+        rospy.spin()
+
+    def import_yaml(self):
+        rospack = rospkg.RosPack()
+        with open(rospack.get_path('ar_tag_detection')+"/config/tags.yaml", 'r') as stream:
+            try:
+                tag_locations=yaml.safe_load(stream)
+                #print(bay_lib)
+            except yaml.YAMLError as exc:
+                print(exc)
+                print("check Global_planner.yaml in config folder")
+            return tag_locations
+    
+    def get_turtlePose(self, msg):
+        # get the pose of turtlebot
+        self.X = msg.pose.pose.position.x
+        self.Y = msg.pose.pose.position.y
+        turtleZrot = msg.pose.pose.orientation.z
+        turtleWrot = msg.pose.pose.orientation.w
+        self.TZ = math.atan2(2*turtleZrot*turtleWrot, 1-2*turtleZrot*turtleZrot)
+ 
+    def get_tags(self, data):
+        try:
+            msg=data.markers[0]
+            id_tag=msg.id
+            AR_x = msg.pose.pose.position.x
+            AR_y = msg.pose.pose.position.y
+            AR_Z = msg.pose.pose.position.z
+            Zrot = msg.pose.pose.orientation.z
+            Wrot = msg.pose.pose.orientation.w
+            AR_tz = math.atan2(2*Zrot*Wrot, 1-2*Zrot*Zrot)
+            tag_location=self.import_yaml()
+
+            Tag_pose=tag_location['AR_tags'][str(id_tag)]
+
+            camera_to_tag=np.array([[math.cos(AR_tz),-math.sin(AR_tz),0,AR_x],[math.sin(AR_tz),math.cos(AR_tz),0,AR_y],[0,0,1,AR_Z],[0,0,0,1]])
+            tag_to_camera=np.linalg.inv(camera_to_tag)
+            map_to_tag=np.array([[math.cos(Tag_pose[5]),-math.sin(Tag_pose[5]),0,Tag_pose[0]],[math.sin(Tag_pose[5]),math.cos(Tag_pose[5]),0,Tag_pose[1]],[0,0,1,Tag_pose[2]],[0,0,0,1]])
+
+            map_to_camera=np.dot(map_to_tag,tag_to_camera)
+
+            msg_pose=Odometry()
+            msg_pose.pose.pose.position.x=map_to_camera[0,3]
+            msg_pose.pose.pose.position.y=map_to_camera[1,3]
+            msg_pose.pose.pose.orientation.z=0.2
+            # msg_pose.pose.pose.orientation.z=math.atan(map_to_camera[1,0]/map_to_camera[0,0])
+            msg_pose.pose.pose.orientation.w=1
+            self.tag_pub.publish(msg_pose)
+            rate = rospy.Rate(50)
+            rate.sleep()
+        except:
+            rospy.loginfo("Tag is not seen")
+
+        '''
+
+        #publish target0 flag on target0_Flag
+        target0_flag = rospy.Publisher("target0_Flag", Bool, queue_size=10)
+        rospy.loginfo("publishing target0 flag set on /taget0_Flag topic")
+        i = 1
+        while i < 50:
+            i += 1
+            target0_flag.publish(self.t0_flag)
+
+
+        rate = rospy.Rate(10)
+        #publish target0 flag on target0_Flag
+        target1_flag = rospy.Publisher("target1_Flag", Bool, queue_size=10)
+        rospy.loginfo("publishing target1 flag set on /taget1_Flag topic")
+        i = 1
+        while i < 50:
+            i += 1
+            target1_flag.publish(self.t1_flag)"""
+
+        if self.t0_flag == True and self.t1_flag == True:
+            goToOn = rospy.Publisher("goToOn_flag", Bool, queue_size=10)
+            goToOn_flag = True
+            i = 1
+            while i < 100:
+                i += 1
+                goToOn.publish(goToOn_flag)
+            return
+        else:
+            goToOn = rospy.Publisher("goToOn_flag", Bool, queue_size=10)
+            goToOn_flag = False
+            goToOn.publish(goToOn_flag)
+            
+
+        #get number of amrkers detected
+        n = len(msg.markers)
+
+        if n == 0:
+            return
+
+        #for each markers (0,1) detected do the following
+        for tag in msg.markers:
+            if tag.id == 0:
+                #counter if required
+                if self.t0_flag == True:
+                    continue
+
+                affine_transf = [[math.cos(turtleAngle), -math.sin(turtleAngle), 0, turtleX], [math.sin(turtleAngle), math.cos(turtleAngle), 0, turtleY], [0, 0, 1, turtleZ], [0, 0, 0, 1]]
+                """affine_transf = numpy.array([[1-2*turtleZrot*turtleZrot, 2*turtleZrot*turtleWrot, 0, turtleX], [-2*turtleZrot*turtleWrot, 1-2*turtleZrot*turtleZrot, 0, turtleY], [0, 0, 1, turtleZ], [0, 0, 0, 1]])"""
+                point_in_baseframe = [[tag.pose.pose.position.x], [tag.pose.pose.position.y], [tag.pose.pose.position.z], [1]]
+                point_in_mapframe = numpy.matmul(affine_transf, point_in_baseframe)
+
+                self.xx0 = self.xx0 + point_in_mapframe[0]
+                self.yy0 = self.yy0 + point_in_mapframe[1]
+                self.zz0 = self.zz0 + point_in_mapframe[2] 
+                self.counter0 += 1
+
+                if self.counter0 == 1:
+                    tag_target0.pose.position.x = self.xx0/1.0
+                    tag_target0.pose.position.y = self.yy0/1.0               
+                    tag_target0.pose.position.z = self.zz0/1.0
+                   
+                    tag_target0.pose.orientation.w = 1
+
+                    #add time stamp and frameid
+                    tag_target0.header.stamp = rospy.Time.now()
+                    tag_target0.header.frame_id = "map"
+                   
+                    self.t0_flag = True
+
+
+
+            elif tag.id == 1:
+                #counter if required
+                if self.t1_flag == True:
+                    continue
+
+                affine_transf = numpy.array([[math.cos(turtleAngle), -math.sin(turtleAngle), 0, turtleX], [math.sin(turtleAngle), math.cos(turtleAngle), 0, turtleY], [0, 0, 1, turtleZ], [0, 0, 0, 1]])
+                """affine_transf = numpy.array([[1-2*turtleZrot*turtleZrot, 2*turtleZrot*turtleWrot, 0, turtleX], [-2*turtleZrot*turtleWrot, 1-2*turtleZrot*turtleZrot, 0, turtleY], [0, 0, 1, turtleZ], [0, 0, 0, 1]])"""
+                point_in_baseframe = numpy.array([[tag.pose.pose.position.x], [tag.pose.pose.position.y], [tag.pose.pose.position.z], [1]])
+                point_in_mapframe = numpy.matmul(affine_transf, point_in_baseframe)
+
+                self.xx1 = self.xx1 + point_in_mapframe[0]
+                self.yy1 = self.yy1 + point_in_mapframe[1]
+                self.zz1 = self.zz1 + point_in_mapframe[2] 
+                self.counter1 += 1
+
+                if self.counter1 == 5:
+                    tag_target1.pose.position.x = self.xx1/5.0
+                    tag_target1.pose.position.y = self.yy1/5.0               
+                    tag_target1.pose.position.z = self.zz1/5.0
+                   
+                    tag_target1.pose.orientation.w = 1
+
+                    #add time stamp and frameid
+                    tag_target1.header.stamp = rospy.Time.now()
+                    tag_target1.header.frame_id = "map"
+                   
+                    self.t1_flag = True
+            else:
+                continue
+        '''
+if __name__ == '__main__':
+    try:
+        Tags()
+        # rospy.spin()
+        # rospy.sleep(0.01)
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Localization node terminated.")
